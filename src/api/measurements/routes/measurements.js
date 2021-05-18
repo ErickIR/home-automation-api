589318dd3e6ddc693a0a091df4b919d482827577
erestituyo <erestituyo@CIN00838>
2021-05-18 09:44:37 -0400

    measurements controller

---
+++ src/api/measurements/routes/measurements.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const controller = require('../controllers/measurements');
+
+const measurementRouter = express.Router();
+
+measurementRouter.get('/', controller.getMeasurementsByType);
+
+measurementRouter.post('/power', controller.createPowerMeasurement);
+
+measurementRouter.post('/volume', controller.createVolumeMeasurement);
+
+measurementRouter.post('/water-flow', controller.createWaterFlowMeasurement);
+
+measurementRouter.post('/weight', controller.createWeightMeasurement);
+
+measurementRouter.delete('/reset', controller.resetCollection);
+
+module.exports = measurementRouter;
