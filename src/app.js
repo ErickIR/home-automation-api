113d1f039929b8bf8110736ddaef3eaf7e362c99
erestituyo <erestituyo@CIN00838>
2021-05-18 08:29:00 -0400

    index page

--- src/app.js
+++ src/app.js
@@ -10,26 +10,9 @@ connectToMongoDb();
 app.use(logger('dev'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  const power = new Power({
-    amperage: 1.0,
-    voltage: 1.0,
-    appaPower: 1.0,
-    current: 1.0,
-    powerFact: 1.0,
-    realPower: 1.0,
-  });
-  const volume = new Volume({ gallons: 1.0 });
-  const waterFlow = new WaterFlow({ flow: 1.0 });
-  const weight = new Weight({ pounds: 1.0 });
-
-  Promise.all([power.save(), volume.save(), waterFlow.save(), weight.save()])
-    .then(() => Measurements.find())
-    .then((result) => {
-      res.json({
-        result,
-      });
-    });
+app.get('/', async (req, res) => {
+  var measurements = await Measurements.find();
+  res.send(measurements)
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const config = require('./config');
 
-const PORT = config.port;
+const PORT = config.port || 3000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
